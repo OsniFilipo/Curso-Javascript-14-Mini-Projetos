e0acaf36fcf33f8ffe9dce15b244cdde20d6e0ff
OsniFilipo <osnifilipo@gmail.com>
2022-01-28 19:48:04 -0300

    13 m

--- Curso Javascript/Variáveis/scripts.js
+++ Curso Javascript/Variáveis/scripts.js
@@ -3,3 +3,9 @@
 //console.log(idade);
 // não se pode usar  número no let ou espaço na variavel let
 
+let cartas = '4';
+console.log('cartas');
+let deck = '40';
+console.log('deck');
+let trap = '10';
+console.log('trap');
